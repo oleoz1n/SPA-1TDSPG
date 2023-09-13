e99389852013fb254beb6321b29a6051c5161859
oleoz1n <leopity0@gmail.com>
2023-09-13 08:45:56 -0300

    Desctruring, inserção e update do editar/produtos

--- src/routes/EditarProduto.jsx
+++ src/routes/EditarProduto.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { listaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
 
@@ -17,39 +17,70 @@ export default function EditarProdutos() {
     img: prodRecuperadoPorId[0].img,
   });
 
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    //Destructuring
+    const { name, value } = event.target;
+    // Inserir os dados no objeto produto através do setProduto({...})
+    setProduto({
+      ...produto,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let indice;
+    indice = listaProdutos.forEach((item, index)=>{
+      if(item.id == produto.id){
+        indice = index
+      }
+    })
+      listaProdutos.splice(indice,1,produto)
+      navigate("/produtos");
+  }
   return (
     <>
       <main>
         <h1>Editar Produtos</h1>
-        <form action="#" method="get">
+        <form action="#" method="get" onSubmit={handleSubmit}>
           <fieldset>
             <legend>Editar Produto</legend>
             <div>
               <label htmlFor="nome">Nome:</label>
               <input
+                onChange={handleChange}
                 type="text"
                 name="nome"
                 id="nome"
-                defaultValue={produto.nome}
+                value={produto.nome}
               />
               <label htmlFor="desc">Descrição</label>
               <input
+                onChange={handleChange}
                 type="text"
                 name="desc"
                 id="desc"
-                defaultValue={produto.desc}
+                value={produto.desc}
               />
               <label htmlFor="preco">Preço:</label>
-              <input 
-              type="number"
-              name="preco"
-              id="preco"
-              defaultValue={produto.preco}
+              <input
+                onChange={handleChange}
+                type="number"
+                name="preco"
+                id="preco"
+                value={produto.preco}
               />
               <button type="submit">Editar</button>
             </div>
           </fieldset>
         </form>
+        <div>
+          <p>Nome: {produto.nome}</p>
+          <p>Preço: {produto.preco}</p>
+          <p>Desc: {produto.desc}</p>
+        </div>
       </main>
     </>
   );
