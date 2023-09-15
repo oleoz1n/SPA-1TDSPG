8f920dda828d2d16f6e35881e168a5daa448ea46
oleoz1n <leopity0@gmail.com>
2023-09-15 11:35:35 -0300

    css / useEffect / useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Cabecalho from './components/Cabecalho'
 import Rodape from './components/Rodape'
+import style from './App.module.css'
+
+
 export default function App() {
     return (
-        <>
+        <div className={style.container}>
             <Cabecalho/>
             <Outlet/> 
             <Rodape/>
-        </>
+        </div>
     )
     // OUtlet = saida dos dados das rotas
 }
