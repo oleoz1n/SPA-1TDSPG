290d424ba170b14cccce36cfc25a4ae2abcca6e6
oleoz1n <leopity0@gmail.com>
2023-10-04 08:59:46 -0300

    Refatorando o projeto retirando items

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,14 @@
 export default function Home(){
+    document.title = "Home"
     return (
         <>
-            <h1>HOME</h1>
+            <h1>Produtos em oferta...</h1>
+            <figure>
+                <figcaption>
+                    Oferta
+                </figcaption>
+                <img src="/online-shopping-640.png" alt="Produtos diversos" />
+            </figure>
         </>
     )
 }
